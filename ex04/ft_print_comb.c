0c9477ecec858ccb128185d5b5b0a5307962c3e0
Mokgadi Tryphina Magongwa <mmagongw@c5r9s6.wethinkcode.co.za>
2020-06-23 14:25:36 +0200

    print comb

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmagongw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 14:08:32 by mmagongw          #+#    #+#             */
+/*   Updated: 2020/06/23 14:24:47 by mmagongw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_print_3_char(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (a < '7')
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	while (a <= '9')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_print_3_char(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
